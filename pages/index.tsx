20e8eaa895eab015cec735af2fbb4a70d7a6ab1e
Sagar Nirwal <62455271+Sagar247776@users.noreply.github.com>
2022-01-28 05:00:14 +0530

    Hero Section Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Hero />
     </div>
   )
 }
