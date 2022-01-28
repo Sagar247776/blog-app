a400cd78e96c9175fbd8029f63a433a43aafe9ce
Sagar Nirwal <62455271+Sagar247776@users.noreply.github.com>
2022-01-29 02:56:17 +0530

    Dark Mode Added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,26 @@
 import Link from "next/link";
+import {useTheme} from 'next-themes'
+import {MoonIcon, SunIcon} from '@heroicons/react/solid'
 
 function Header() {
+
+  let {systemTheme, theme, setTheme} = useTheme()
+  let renderThemeChanger =() =>{
+  let currentTheme = theme === 'system' ? systemTheme : theme;
+  if (currentTheme === 'dark'){
+    return (
+      <SunIcon className="w-7 h-7" role="button" onClick={() => setTheme
+      ('light')} />
+    )
+  }
+  else{
+    return (
+      <MoonIcon className="w-7 h-7" role="button" onClick={() => setTheme
+      ('dark')} />
+    )
+  }
+}
+
   return <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
           <Link href="/">
@@ -12,9 +32,9 @@ function Header() {
               <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
           </div>      
       </div>
-      <div className="flex items-center space-x-5 text-green-600 px-4">
-          <h3>Sing In</h3>
-          <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3>
+      <div className="flex items-center space-x-5 px-4">
+          <div>{renderThemeChanger()}</div>
+          <h3 className="text-green-600">Sing In</h3>
       </div>
 
   </header>;
